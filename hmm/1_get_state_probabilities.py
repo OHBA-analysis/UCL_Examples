2d1141589d1b45db71a6fb807f2e29c640122d45
Chetan Gohil <chetan.gohil@psych.ox.ac.uk>
2024-02-20 13:27:45 +0000

    Save state time courses in matlab format.

--- hmm/1_get_state_probabilities.py
+++ hmm/1_get_state_probabilities.py
@@ -5,14 +5,17 @@
 # Authors: Chetan Gohil <chetan.gohil@psych.ox.ac.uk>
 
 import os
+import mne
 import numpy as np
+from scipy import io
 
 from osl_dynamics.data import Data
 from osl_dynamics.models import load
+from osl_dynamics.inference import modes
 
 subject = "LN_VTA2"
 
-# Load data
+# Load sign flipped data
 data = Data(f"../preproc/data/npy/{subject}.npy")
 
 # Prepare data
@@ -28,9 +31,26 @@ model = load("../camcan_norm_model")
 # Get state probabilities
 alpha = model.get_alpha(data)
 
-# Save
+# Make output directory
 os.makedirs(f"hmm/{subject}", exist_ok=True)
-np.save(f"hmm/{subject}/alpha.npy", alpha)
+
+# Save in python format
+#np.save(f"hmm/{subject}/alpha.npy", alpha)
+
+# Convert to MNE raw object
+source_raw = mne.io.read_raw_fif(
+    f"../preproc/data/src/{subject}/parc/parc-raw.fif",
+    verbose=False,
+)  # note: this is not sign flipped
+alpha_raw = modes.convert_to_mne_raw(alpha, source_raw, n_embeddings=15)
+
+# Get data (including bad segments as 0 values)
+X = source_raw.get_data(picks="misc")
+P = alpha_raw.get_data()
+X = {"X": X, "P": P}
+
+# Save in Matlab format
+io.savemat(f"hmm/{subject}/X.mat", X)
 
 # Delete temporary directory
 data.delete_dir()
